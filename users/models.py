ff041f546c8efa02afa11230bb6335c7f7d169b1
Foys17 <mhfoysal17@gmail.com>
2025-11-27 12:22:34 +0600

    All models created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+USER_ROLES = (
+    ('admin', 'admin'),
+    ('teacher', 'Teacher'),
+    ('student', 'Student'),
+)
+
+class User(AbstractUser):
+    role = models.CharField(max_length = 10,choices = USER_ROLES)
+    mobile_no = models.CharField(max_length=15,blank = True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
